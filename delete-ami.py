a27b75d1ffff5351ccb049fbaa20b2d7cbc5dc16
Storm Galloway <storm.galloway@variq.com>
2020-10-29 11:24:21 -0400

    added deletion of snapshots

--- delete-ami.py
+++ delete-ami.py
@@ -1,20 +1,41 @@
 import json
 import boto3
 import datetime
+import time
 
 def lambda_handler(event, context):
     ec = boto3.client('ec2')
     
     delete_date = datetime.date.today() 
     delete_fmt = delete_date.strftime('%m-%d-%Y')
+    snapshots = []
+    
     
     images = ec.describe_images(Filters=[
-            {
-                'Name': 'tag:DeleteOn',
-                'Values': [delete_fmt]
-            }],Owners=['self'])
+        {
+            'Name': 'tag:DeleteOn',
+            'Values': [delete_fmt]
+        }],Owners=['self'])
+    
+    #pull snapshot ids into an array
+    ebs_mapping = images['Images'][0]['BlockDeviceMappings']
+    print(ebs_mapping)
+    for snapshot in ebs_mapping:
+        if 'Ebs' in snapshot:
+            snapshots.append(snapshot['Ebs']['SnapshotId'])
+        else:
+            continue
     
     for image in images['Images']:
+        print(image)
         response = ec.deregister_image(
         ImageId=image['ImageId']
-    )
+        )
+    time.sleep(10)
+        
+        #delete snapshots based off snapshot ID array
+    for snapshot in snapshots:
+        print(snapshot)
+        response = ec.delete_snapshot(
+            SnapshotId = snapshot
+        )
