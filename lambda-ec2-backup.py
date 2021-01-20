47a7a02f19460dc38140ac9eb0f3c61d7fce12c1
Storm Galloway <storm.galloway@variq.com>
2021-01-20 11:59:48 -0500

    fixed error

--- lambda-ec2-backup.py
+++ lambda-ec2-backup.py
@@ -6,78 +6,51 @@
 # After creating the AMI it creates a "DeleteOn" tag on the AMI indicating when
 # it will be deleted using the Retention value and another Lambda function
 
+import json
 import boto3
-import collections
 import datetime
-import sys
-import pprint
-
-ec = boto3.client('ec2')
-#image = ec.Image('id')
+import time
 
 def lambda_handler(event, context):
-    backup_on = event['Backup']
-    retention = event['DeleteOn']
-    reservations = ec.describe_instances(Filters=[
+    ec = boto3.client('ec2')
+    
+    delete_date = datetime.date.today() 
+    delete_fmt = delete_date.strftime('%m-%d-%Y')
+    snapshots = []
+    
+    
+    images = ec.describe_images(Filters=[
         {
-            'Name': 'tag:Backup',
-            'Values': [backup_on]
-        },
-    ]).get('Reservations', [])
-
-    instances = sum([[i for i in r['Instances']] for r in reservations], [])
-
-    print("Found %d instances that need backing up" % len(instances))
-
-    to_tag = collections.defaultdict(list)
-
-    for instance in instances:
-        retention_days = retention
-        server_name = ''
-
-        #create_image(instance_id, name, description=None, no_reboot=False, block_device_mapping=None, dry_run=False)
-        # DryRun, InstanceId, Name, Description, NoReboot, BlockDeviceMappings
-        create_time = datetime.datetime.now()
-        create_fmt = create_time.strftime('%Y-%m-%d')
-        
-        for tags in instance['Tags']:
-            if tags['Key'] == 'Name':
-                server_name = tags['Value']
-            
-        AMIid = ec.create_image(
-            InstanceId=instance['InstanceId'],
-            Name="Backup - " + server_name + " from " +
-            create_fmt,
-            Description="Lambda created AMI of instance " +
-            instance['InstanceId'] + " from " + create_fmt,
-            NoReboot=True,
-            DryRun=False)
-
-        pprint.pprint(instance)
-
-        to_tag[retention_days].append(AMIid['ImageId'])
-
-        print("Retaining AMI %s of instance %s for %d days" % (
-            AMIid['ImageId'],
-            instance['InstanceId'],
-            retention_days,
-        ))
-
-    print(to_tag.keys())
-
-    for retention_days in to_tag.keys():
-        delete_date = datetime.date.today() + datetime.timedelta(
-            days=retention_days)
-        delete_fmt = delete_date.strftime('%m-%d-%Y')
-        print("Will delete %d AMIs on %s" %
-              (len(to_tag[retention_days]), delete_fmt))
-
-        #break
-
-        ec.create_tags(Resources=to_tag[retention_days],
-                       Tags=[
-                           {
-                               'Key': 'DeleteOn',
-                               'Value': delete_fmt
-                           },
-                       ])
+            'Name': 'tag:DeleteOn',
+            'Values': [delete_fmt]
+        }],Owners=['self'])
+    
+    #pull snapshot ids into an array
+    length_array = len(images['Images'])
+    print(str(length_array) + " AMI's need to be deleted")
+    increment = 0
+    while increment < length_array:
+        ebs_mapping = images['Images'][increment]['BlockDeviceMappings']
+        print(ebs_mapping)
+        for snapshot in ebs_mapping:
+            if 'Ebs' in snapshot:
+                snapshots.append(snapshot['Ebs']['SnapshotId'])
+                print(snapshots)
+            else:
+                continue
+        increment += 1
+    
+    for image in images['Images']:
+        print(image)
+        response = ec.deregister_image(
+        ImageId=image['ImageId']
+        )
+    time.sleep(10)
+       
+        #delete snapshots based off snapshot ID array
+    for snapshot in snapshots:
+        print(snapshot)
+        response = ec.delete_snapshot(
+            SnapshotId = snapshot
+        )
+        print(snapshot + "has been deleted")
